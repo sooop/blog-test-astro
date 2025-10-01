da2070cb0e8f13bfd02d482b4f95989fdad3b42c
sooop <soooprmx@gmail.com>
2025-10-02 00:30:19 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { katexDisplayFix } from './katex-display-fix.mjs';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -14,9 +16,12 @@ export default defineConfig({
       shikiConfig: { theme: 'vitesse-light',
         wrap: true,
        }
-	},
+    },
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://soooprmx.com',
+  integrations: [sitemap()]
 });
